da1ca004b5c159d405d956bb9606382df6b056d3
anlutkov <anlutkov@gmail.com>
2020-09-22 09:39:10 +0300

    comment 7 on master

--- task_3.cpp
+++ task_3.cpp
@@ -4,4 +4,5 @@ int main {
     cout << "Hello, World! (on master 2)" <<Endl;
     cout << "Hello, World! (on raiting) 6" <<Endl;
     cout << "Hello, World! (on raiting) 5" <<Endl;
+     cout << "Hello, World! (on comment) 7" <<Endl;
 }
