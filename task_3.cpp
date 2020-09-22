38ebf8451a625794515c03dd32024567b71e3021
anlutkov <anlutkov@gmail.com>
2020-09-22 11:50:39 +0300

    final commit on master

--- task_3.cpp
+++ task_3.cpp
@@ -3,7 +3,5 @@ using namespace std;
 int main {  
     cout << "Hello, World! (on master 2)" <<Endl;
     cout << "Hello, World! (on raiting) 6" <<Endl;
-    cout << "Hello, World! (on raiting) 5" <<Endl;
-     cout << "Hello, World! (on comment) 7" <<Endl;
-    cout << "Hello, World! (on commet) 3" <<Endl;
+    cout << "Hello, World! (on comment) 7" <<Endl;
 }
